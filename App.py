00094d21fa27eb6a9e5d1eb273ac4061098acd34
Rabeya-MeghOps <rabeya@meghops.io>
2026-01-07 12:57:23 +0600

    Add initial Playwright browser launch script

--- App.py
+++ App.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
-    browser=playwright.chromium.launch(headless=False,slow_mo=500)
+    browser=playwright.chromium.launch(headless=False,slow_mo=1000)
     #create a new page
     page=browser.new_page()
-
+    # visit the Playwright website 
+    page.goto("https://playwright.dev/python")
 
     browser.close()
