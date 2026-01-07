bedeea0bdb4110c5f1894b32d1a20231df2f992e
Rabeya-MeghOps <rabeya@meghops.io>
2026-01-07 13:06:12 +0600

    Add navigation to Docs section using get_by_role locator

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
-    browser=playwright.chromium.launch(headless=False,slow_mo=1000)
+    browser=playwright.chromium.launch(headless=False,slow_mo=1200)
     #create a new page
     page=browser.new_page()
     # visit the Playwright website 
     page.goto("https://playwright.dev/python")
+
+    # Locate a link element with "Docs" text
+    docs_button=page.get_by_role('link',name="Docs")
+    docs_button.click()
 
     browser.close()
